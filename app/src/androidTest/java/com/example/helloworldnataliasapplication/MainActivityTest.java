c6857a89d5b3c494add8f0a1ccfd52bd6ac8f1a4
NataliaGourova <46657216+nataliagourova@users.noreply.github.com>
2021-04-12 23:06:47 -0700

    added menu and test

--- app/src/androidTest/java/com/example/helloworldnataliasapplication/MainActivityTest.java
+++ app/src/androidTest/java/com/example/helloworldnataliasapplication/MainActivityTest.java
@@ -1,11 +1,25 @@
 package com.example.helloworldnataliasapplication;
 
-import static org.junit.Assert.*;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-@RunWith()
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
 
-    @Rule()
-    ...
+    @Test
+    public void checkHelloWorldDisplayed() {
+        onView(withId(R.id.helloWorldtext)).check(matches(isDisplayed()));
+    }
 }
--- app/src/main/java/com/example/helloworldnataliasapplication/MainActivity.java
+++ app/src/main/java/com/example/helloworldnataliasapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.helloworldnataliasapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main_menu, menu);
+        return true;
     }
 }

c40197db0f3ded7d81074049375e010ad09aaf54
NataliaGourova <46657216+nataliagourova@users.noreply.github.com>
2021-04-27 22:14:20 -0700

    Layout the profile activity

--- app/src/main/java/com/example/helloworldnataliasapplication/SubmitConfirmActivity.java
+++ app/src/main/java/com/example/helloworldnataliasapplication/SubmitConfirmActivity.java
@@ -11,19 +11,35 @@ public class SubmitConfirmActivity extends AppCompatActivity {
 
     static final int RESULT_BACK = 1;
 
+    TextView dobLabel;
+    TextView nameLabel;
+    TextView occupationLabel;
+    TextView descriptionLabel;
+    Button backButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit_confirm);
+        findViews();
+
         Intent intent = getIntent();
-        String name = intent.getStringExtra(MainActivity.NAME_KEY);
-        String welcomeText = getString(R.string.thanks_signup, name);
-        TextView welcomeLabel = findViewById(R.id.welcome_label);
-        welcomeLabel.setText(welcomeText);
-        Button backButton = findViewById(R.id.back_button);
+        nameLabel.setText(intent.getStringExtra(MainActivity.NAME_KEY));
+        dobLabel.setText(intent.getStringExtra(MainActivity.DOB_KEY));
+        occupationLabel.setText(intent.getStringExtra(MainActivity.OCCUPATION_KEY));
+        descriptionLabel.setText(intent.getStringExtra(MainActivity.DESCRIPTION_KEY));
+
         backButton.setOnClickListener(v -> {
             setResult(RESULT_BACK);
             finish();
         });
+    }
+
+    private void findViews() {
+        dobLabel = findViewById(R.id.dob_profile_label);
+        nameLabel = findViewById(R.id.name_profile_label);
+        descriptionLabel = findViewById(R.id.description_profile_label);
+        occupationLabel = findViewById(R.id.occupation_profile_label);
+        backButton = findViewById(R.id.back_button);
     }
 }

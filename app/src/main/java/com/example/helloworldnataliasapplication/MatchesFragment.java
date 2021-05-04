02ff7ab68918be8a524cb5143235ee55f3d5b4c9
NataliaGourova <46657216+nataliagourova@users.noreply.github.com>
2021-05-03 23:15:31 -0700

    Add empty Matches and Settings fragments

---
+++ app/src/main/java/com/example/helloworldnataliasapplication/MatchesFragment.java
@@ -0,0 +1,33 @@
+package com.example.helloworldnataliasapplication;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MatchesFragment extends Fragment {
+    public MatchesFragment() {
+        // Required empty public constructor
+    }
+
+    public static MatchesFragment newInstance() {
+        MatchesFragment fragment = new MatchesFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_matches, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/helloworldnataliasapplication/SettingsFragment.java
@@ -0,0 +1,33 @@
+package com.example.helloworldnataliasapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class SettingsFragment extends Fragment {
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    public static SettingsFragment newInstance() {
+        SettingsFragment fragment = new SettingsFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+}
